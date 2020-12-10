c03cdccee1526d6a272744b4026adb67c00ab06c
Calle Tornborg <calle.tornborg@gmail.com>
2020-12-10 10:37:11 +0100

    Add project files.

---
+++ AssetProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AssetProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hey Guys!");
+        }
+    }
+}
