f4990d033940df942eb40614563105d696066af0
Calle Tornborg <calle.tornborg@gmail.com>
2020-12-11 11:41:54 +0100

    Added a Class for Ester Egg ;)

---
+++ AssetProject/EasterEgg1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace EFAssets
+{
+    public class EasterEgg1
+    {
+        public void ShowEasterEgg()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("     MERRY CHRISTMAS TO ALL OF YOU");
+            Console.WriteLine("           & HAPPY CODING!");
+            Console.WriteLine("");
+            Console.WriteLine("                .------ -.");
+            Console.WriteLine("      *         | #     |      *");
+            Console.WriteLine("                | _____ |  *");
+            Console.WriteLine("               _|=======|_     *");
+            Console.WriteLine("   *          [___________]");
+            Console.WriteLine("               .'       `.   *   | /");
+            Console.WriteLine("              :   @   @   :     " + (char)92 + "|/ _");
+            Console.WriteLine("              :     <     :     //");
+            Console.WriteLine("               `._`...'_.'     //      *");
+            Console.WriteLine("       *   `/`'s/'''''    `-.  //");
+            Console.WriteLine("           /'/s/   _        `//");
+            Console.WriteLine("          // s/   (_)       /");
+            Console.WriteLine("         ///s/     _        :  *");
+            Console.WriteLine("    *   ///s/     (_)      .'          *");
+            Console.WriteLine("       // |/`.           .'");
+            Console.WriteLine("     _//    .-''-------'`-.");
+            Console.WriteLine("      /|" + (char)92 + "  .'       _       `.      *");
+            Console.WriteLine("     / | " + (char)92 + ":        (_)        :");
+            Console.WriteLine("         :          _:");
+            Console.WriteLine("         :         (_)         :");
+            Console.WriteLine("  *      :          _: *");
+            Console.WriteLine("         :         (_)         :");
+            Console.WriteLine("          :                   :");
+            Console.WriteLine("           :                 :");
+            Console.WriteLine("             `-._       _.- '");
+            Console.WriteLine("               `'''''''''");
+            Console.WriteLine("");
+
+        }
+    }
+}
